212ea253d59a6469e114e959ccdfc8313cfdc332
Beatriz Miranda <beatriz.miranda@acct.global>
2020-11-16 21:31:58 -0300

    feat(initial screen): code improvements, separete parts in small widgets

--- lib/views/initialScreen.dart
+++ lib/views/initialScreen.dart
@@ -19,7 +19,7 @@ class _InitialScreenState extends State<InitialScreen> {
       Stack(
         children: <Widget>[
           ClipRRect(
-            borderRadius: BorderRadius.only(bottomLeft:  Radius.circular(30), bottomRight:  Radius.circular(30)),
+            borderRadius: BorderRadius.only(bottomLeft:  Radius.circular(15), bottomRight:  Radius.circular(15)),
             child: Image.asset(
               "src/img/InitialScreen.png",
               fit: BoxFit.cover,
@@ -30,54 +30,60 @@ class _InitialScreenState extends State<InitialScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Card(
-                  margin: EdgeInsets.all(20),
-                  elevation: 5,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 30, 20, 30),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Column(
-                          children: [
-                            Text(
-                              "Para onde você quer ir?", 
-                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25)
-                            ),
-                            Container(
-                              margin: EdgeInsets.only(top: 50),
-                              child: RaisedButton(
-                                onPressed: () {
-                                  Navigator.of(context).pushReplacementNamed('home');
-                                },
-                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                                padding: EdgeInsets.all(0.0),
-                                child: Ink(
-                                  decoration: BoxDecoration(
-                                      gradient: LinearGradient(colors: [Color(0xFFFFA251), Color(0xFFFFB162)]),
-                                      borderRadius: BorderRadius.circular(10)
-                                  ),
-                                  child: Container(
-                                    constraints: BoxConstraints(maxWidth: 330.0, minHeight: 70.0),
-                                    alignment: Alignment.center,
-                                    child: Text(
-                                      "Agende sua viagem",
-                                      style: TextStyle(fontSize: 24, color: Colors.white),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+                _card(),
               ],
             ),
           )            
         ]
       );
+  }
+
+  Widget _card() {
+    return Card(
+      margin: EdgeInsets.all(20),
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 30, 20, 30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Column(
+              children: [
+                Text(
+                  "Para onde você quer ir?", 
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25)
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 50),
+                  child: _callToActionButton()
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _callToActionButton() {
+    return RaisedButton(
+      onPressed: () {},
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      padding: EdgeInsets.all(0.0),
+      child: Ink(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [Color(0xFFFFA251), Color(0xFFFFB162)]),
+            borderRadius: BorderRadius.circular(10)
+        ),
+        child: Container(
+          constraints: BoxConstraints(maxWidth: 330.0, minHeight: 70.0),
+          alignment: Alignment.center,
+          child: Text(
+            "Agende sua viagem",
+            style: TextStyle(fontSize: 24, color: Colors.white),
+          ),
+        ),
+      ),
+    );
   }
 }
