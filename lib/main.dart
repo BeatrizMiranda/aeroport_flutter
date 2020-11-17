372e2cfa60069dc6c80034eeb344117994ab17ad
Beatriz Miranda <beatriz.miranda@acct.global>
2020-11-16 21:06:36 -0300

    feat(initial screen): creation of the initial screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:airport/views/home.dart';
+import 'package:airport/views/initialScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  bool isLoged = false;
+
   runApp(MaterialApp(
     title: 'Time To Travel',
-    home: Home(),
+    home: isLoged ? Home() : InitialScreen(),
     theme: ThemeData(
       hintColor: Color(0xFFFFA251),
       primaryColor: Colors.white,
@@ -11,99 +15,3 @@ void main() {
   ));
 }
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-
-  bool isAdmin = true;
-
-  Map<String, IconData> normalUser = {
-    'Minha Conta': Icons.account_circle,
-    'Minhas Viagens': Icons.card_travel
-  };
-
-  Map<String, IconData> adminUser = {
-    'Usuários': Icons.supervised_user_circle,
-    'Minha Conta': Icons.account_circle,
-    'Voos': Icons.flight_takeoff,
-    'Companhia': Icons.flight
-  };
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _body(),
-      bottomNavigationBar: _footer(),
-      floatingActionButton: _footerFloatingBtn(),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      backgroundColor: Color(0xFFF2F2F2),
-    );
-  }
-
-  Widget _body() {
-    return SingleChildScrollView(
-      padding: EdgeInsets.symmetric(horizontal: 15),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-        ],
-      ),
-    );
-  }
-
-  Widget getFooterIcons(Map<String, IconData> footerItems) {
-    List<Widget> list = List<Widget>();
-    footerItems.forEach((message, icon) => list.add(_footerItem(message, icon)));
-    return Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: list);
-  }
-
-  Widget _footer() {
-    return BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        child: Container(
-          height: 70.0, 
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(25, 0, 25, 5),
-            child: isAdmin ? getFooterIcons(adminUser) : getFooterIcons(normalUser)
-          ), 
-        ),
-      );
-  }
-
-  Widget _footerItem(text, icon) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        IconButton(onPressed: () {}, icon: Icon(icon, size: 30, color: Color(0xFF333333))),
-        Text(text)
-      ]);
-  }
-
-  Widget _footerFloatingBtn() {
-    return Container(
-          height: 70,
-          width: 70,
-          child: FittedBox(
-            child: FloatingActionButton(
-            onPressed: () => {},
-            tooltip: 'Tela de Início',
-            child: Container(
-                width: 70,
-                height: 70,
-                child: Icon(Icons.flight, size: 35),
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    gradient: LinearGradient(colors: [
-                      Color(0xFFFF5929), 
-                      Color(0xFFFFB162)
-                    ])
-                  ),
-            ),
-          ),
-        ),
-      );
-  }
-}
---
+++ lib/views/initialScreen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class InitialScreen extends StatefulWidget {
+  @override
+  _InitialScreenState createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _body(),
+      backgroundColor: Color(0xFFF2F2F2),
+    );
+  }
+
+  Widget _body() {
+    return 
+      Stack(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.only(bottomLeft:  Radius.circular(30), bottomRight:  Radius.circular(30)),
+            child: Image.asset(
+              "src/img/InitialScreen.png",
+              fit: BoxFit.cover,
+              height: 650
+            ),
+          ),
+          Positioned(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Card(
+                  margin: EdgeInsets.all(20),
+                  elevation: 5,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 30, 20, 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Column(
+                          children: [
+                            Text(
+                              "Para onde você quer ir?", 
+                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25)
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(top: 50),
+                              child: RaisedButton(
+                                onPressed: () {
+                                  Navigator.of(context).pushReplacementNamed('home');
+                                },
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                padding: EdgeInsets.all(0.0),
+                                child: Ink(
+                                  decoration: BoxDecoration(
+                                      gradient: LinearGradient(colors: [Color(0xFFFFA251), Color(0xFFFFB162)]),
+                                      borderRadius: BorderRadius.circular(10)
+                                  ),
+                                  child: Container(
+                                    constraints: BoxConstraints(maxWidth: 330.0, minHeight: 70.0),
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      "Agende sua viagem",
+                                      style: TextStyle(fontSize: 24, color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )            
+        ]
+      );
+  }
+}
