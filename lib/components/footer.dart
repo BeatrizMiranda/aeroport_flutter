a4d2a0ad6ae93a2c74c159a2d64a0d5afeef508c
rodriigorgm <rodriigorgm@hotmail.com>
2020-11-19 11:38:41 -0300

    teste

--- lib/components/footer.dart
+++ lib/components/footer.dart
@@ -10,8 +10,8 @@ class Footer extends StatefulWidget {
 }
 
 class _Footer extends State<Footer> {
-
   bool isAdmin = false;
+  bool rodrigo = false;
 
   Map<String, IconData> normalUser = {
     'Minha Conta': Icons.account_circle,
@@ -25,39 +25,40 @@ class _Footer extends State<Footer> {
     'Companhia': Icons.flight
   };
 
-  
   @override
   Widget build(BuildContext context) {
     return BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        child: Container(
-          height: 70.0, 
-          child: Padding(
+      shape: const CircularNotchedRectangle(),
+      child: Container(
+        height: 70.0,
+        child: Padding(
             padding: EdgeInsets.fromLTRB(25, 0, 25, 5),
-            child: isAdmin ? getFooterIcons(adminUser) : getFooterIcons(normalUser)
-          ), 
-        ),
-      );
+            child: isAdmin
+                ? getFooterIcons(adminUser)
+                : getFooterIcons(normalUser)),
+      ),
+    );
   }
 
   Widget getFooterIcons(Map<String, IconData> footerItems) {
     List<Widget> list = List<Widget>();
-    footerItems.forEach((message, icon) => list.add(_footerItem(message, icon)));
-    return Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: list);
+    footerItems
+        .forEach((message, icon) => list.add(_footerItem(message, icon)));
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween, children: list);
   }
 
   Widget _footerItem(text, icon) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        IconButton(onPressed: () {}, icon: Icon(icon, size: 30, color: Palette.lightBlack)),
-        Text(text)
-      ]);
+    return Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+      IconButton(
+          onPressed: () {},
+          icon: Icon(icon, size: 30, color: Palette.lightBlack)),
+      Text(text)
+    ]);
   }
 }
 
 class FooterFloatingBtn extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -65,19 +66,17 @@ class FooterFloatingBtn extends StatelessWidget {
       width: 70,
       child: FittedBox(
         child: FloatingActionButton(
-          onPressed: () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Home())),
+          onPressed: () => Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => Home())),
           tooltip: 'Tela de Início',
           child: Container(
             width: 70,
             height: 70,
             child: Icon(Icons.flight, size: 35),
             decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              gradient: LinearGradient(colors: [
-                Palette.darkOrange, 
-                Palette.lightOrange
-              ])
-            ),
+                shape: BoxShape.circle,
+                gradient: LinearGradient(
+                    colors: [Palette.darkOrange, Palette.lightOrange])),
           ),
         ),
       ),
