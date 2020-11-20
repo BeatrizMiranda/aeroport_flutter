8d8ceabbdd05c28ffff28052b04e88bd05f7aff4
Beatriz Miranda <beatriz.miranda@acct.global>
2020-11-19 22:22:04 -0300

    fix(footer): create an class for footer icons

--- lib/components/footer.dart
+++ lib/components/footer.dart
@@ -2,6 +2,27 @@ import 'package:airport/layout/pallets.dart';
 import 'package:airport/views/home.dart';
 import 'package:flutter/material.dart';
 
+class FooterIcon {
+  const FooterIcon({this.name, this.icon, this.goToComponent});
+
+  final String name;
+  final IconData icon;
+  final Type goToComponent;
+}
+
+const List<FooterIcon> normalUser = const <FooterIcon>[
+  const FooterIcon(name: 'Minha Conta', icon: Icons.account_circle, goToComponent: Home),
+  const FooterIcon(name: 'Minhas Viagens', icon: Icons.card_travel, goToComponent: Home),
+];
+
+const List<FooterIcon> adminUser = const <FooterIcon>[
+  const FooterIcon(name: 'Usuários', icon: Icons.supervised_user_circle, goToComponent: Home),
+  const FooterIcon(name: 'Minha Conta', icon: Icons.account_circle, goToComponent: Home),
+  const FooterIcon(name: 'Voos', icon: Icons.flight_takeoff, goToComponent: Home),
+  const FooterIcon(name: 'Companhia', icon: Icons.flight, goToComponent: Home),
+];
+
+
 class Footer extends StatefulWidget {
   Footer({Key key}) : super(key: key);
 
@@ -11,20 +32,7 @@ class Footer extends StatefulWidget {
 
 class _Footer extends State<Footer> {
 
-  bool isAdmin = false;
-
-  Map<String, IconData> normalUser = {
-    'Minha Conta': Icons.account_circle,
-    'Minhas Viagens': Icons.card_travel
-  };
-
-  Map<String, IconData> adminUser = {
-    'Usuários': Icons.supervised_user_circle,
-    'Minha Conta': Icons.account_circle,
-    'Voos': Icons.flight_takeoff,
-    'Companhia': Icons.flight
-  };
-
+  bool isAdmin = true;
   
   @override
   Widget build(BuildContext context) {
@@ -40,19 +48,13 @@ class _Footer extends State<Footer> {
       );
   }
 
-  Widget getFooterIcons(Map<String, IconData> footerItems) {
-    List<Widget> list = List<Widget>();
-    footerItems.forEach((message, icon) => list.add(_footerItem(message, icon)));
-    return Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: list);
-  }
-
-  Widget _footerItem(text, icon) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        IconButton(onPressed: () {}, icon: Icon(icon, size: 30, color: Palette.lightBlack)),
-        Text(text)
-      ]);
+  Widget getFooterIcons(List<FooterIcon> footerItems) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween, 
+      children: List.generate(footerItems.length, (index) {
+          return FooterOption(footerIcon: footerItems[index]);
+      })
+    );
   }
 }
 
@@ -82,5 +84,20 @@ class FooterFloatingBtn extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class FooterOption extends StatelessWidget {
+  const FooterOption({Key key, this.footerIcon}) : super(key: key);
+  final FooterIcon footerIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        IconButton(onPressed: () {}, icon: Icon(footerIcon.icon, size: 30, color: Palette.lightBlack)),
+        Text(footerIcon.name)
+      ]);
   }
 }
