ff488c92b5d2b1a8093cc950e517ef9f0d8b22c7
Beatriz Miranda <beatriz.miranda@acct.global>
2020-11-24 13:04:10 -0300

    feat(search result): search result initial layout

--- lib/views/searchResult.dart
+++ lib/views/searchResult.dart
@@ -1,6 +1,122 @@
 import 'package:airport/components/footer.dart';
 import 'package:airport/layout/pallets.dart';
+import 'package:airport/views/MyTrips.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+const List<FlightInfo> userFlights = const <FlightInfo>[
+  FlightInfo(
+    destination: "São Paulo", 
+    shipment: "Rio de Janeiro", 
+    ship_date: "2020-07-02T03:00:00.000Z", 
+    ship_time: "18:00:00", 
+    estimated_time: "03:00:00", 
+    limit:46, 
+    airline_id: 1, 
+    status: "ativo", 
+    image: "https://cdn.pixabay.com/photo/2017/01/08/19/30/rio-de-janeiro-1963744_1280.jpg" 
+  ),
+  FlightInfo(
+    shipment: "São Paulo", 
+    destination: "Rio de Janeiro", 
+    ship_date: "2020-07-02T03:00:00.000Z", 
+    ship_time: "18:00:00", 
+    estimated_time: "03:00:00", 
+    limit:46, 
+    airline_id: 1, 
+    status: "ativo", 
+    image: "https://cdn.pixabay.com/photo/2017/01/08/19/30/rio-de-janeiro-1963744_1280.jpg" 
+  ),
+  FlightInfo(
+    shipment: "São Paulo", 
+    destination: "Rio de Janeiro", 
+    ship_date: "2020-07-02T03:00:00.000Z", 
+    ship_time: "18:00:00", 
+    estimated_time: "03:00:00", 
+    limit:46, 
+    airline_id: 1, 
+    status: "ativo", 
+    image: "https://cdn.pixabay.com/photo/2017/01/08/19/30/rio-de-janeiro-1963744_1280.jpg" 
+  ),
+  FlightInfo(
+    shipment: "São Paulo", 
+    destination: "Rio de Janeiro", 
+    ship_date: "2020-07-02T03:00:00.000Z", 
+    ship_time: "18:00:00", 
+    estimated_time: "03:00:00", 
+    limit:46, 
+    airline_id: 1, 
+    status: "ativo", 
+    image: "https://cdn.pixabay.com/photo/2017/01/08/19/30/rio-de-janeiro-1963744_1280.jpg" 
+  ),
+  FlightInfo(
+    shipment: "São Paulo", 
+    destination: "Rio de Janeiro", 
+    ship_date: "2020-07-02T03:00:00.000Z", 
+    ship_time: "18:00:00", 
+    estimated_time: "03:00:00", 
+    limit:46, 
+    airline_id: 1, 
+    status: "ativo", 
+    image: "https://cdn.pixabay.com/photo/2017/01/08/19/30/rio-de-janeiro-1963744_1280.jpg" 
+  ),
+  FlightInfo(
+    shipment: "São Paulo", 
+    destination: "Rio de Janeiro", 
+    ship_date: "2020-07-02T03:00:00.000Z", 
+    ship_time: "18:00:00", 
+    estimated_time: "03:00:00", 
+    limit:46, 
+    airline_id: 1, 
+    status: "ativo", 
+    image: "https://cdn.pixabay.com/photo/2017/01/08/19/30/rio-de-janeiro-1963744_1280.jpg" 
+  ),
+  FlightInfo(
+    shipment: "São Paulo", 
+    destination: "Rio de Janeiro", 
+    ship_date: "2020-07-02T03:00:00.000Z", 
+    ship_time: "18:00:00", 
+    estimated_time: "03:00:00", 
+    limit:46, 
+    airline_id: 1, 
+    status: "ativo", 
+    image: "https://cdn.pixabay.com/photo/2017/01/08/19/30/rio-de-janeiro-1963744_1280.jpg" 
+  ),
+  FlightInfo(
+    shipment: "São Paulo", 
+    destination: "Rio de Janeiro", 
+    ship_date: "2020-07-02T03:00:00.000Z", 
+    ship_time: "18:00:00", 
+    estimated_time: "03:00:00", 
+    limit:46, 
+    airline_id: 1, 
+    status: "ativo", 
+    image: "https://cdn.pixabay.com/photo/2017/01/08/19/30/rio-de-janeiro-1963744_1280.jpg" 
+  ),
+];
+
+class FlightSearch {
+  const FlightSearch({
+    this.destination, 
+    this.shipment, 
+    this.ship_date, 
+    this.quantity
+  });
+
+  final String shipment;
+  final String ship_date;
+  final String destination;
+  final int quantity;
+}
+
+
+const FlightSearch flightsOptions = FlightSearch(
+  destination: "São Paulo", 
+  shipment: "Rio de Janeiro", 
+  ship_date: "2020-07-02T03:00:00.000Z", 
+  quantity: 5
+);
+
 
 class SearchResult extends StatefulWidget {
   SearchResult({Key key}) : super(key: key);
@@ -9,6 +125,8 @@ class SearchResult extends StatefulWidget {
   _SearchResultState createState() => _SearchResultState();
 }
 class _SearchResultState extends State<SearchResult> {
+  
+  String shipDateFormated = DateFormat('dd/MM/y').format(DateTime.parse(flightsOptions.ship_date));
 
   @override
   Widget build(BuildContext context) {
@@ -31,10 +149,53 @@ class _SearchResultState extends State<SearchResult> {
                 borderRadius: BorderRadius.only(bottomLeft:  Radius.circular(15), bottomRight:  Radius.circular(15)),
                 child: Image.asset(
                   "src/img/searchOpacity.png",
-                  fit: BoxFit.cover,
-                  height: 325,
+                  fit: BoxFit.contain,
+                  height: 220,
                 ),
               ),
+              Positioned.fill(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    padding: EdgeInsets.fromLTRB(30, 55, 30, 55),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              flightsOptions.shipment, 
+                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22)
+                            ),
+                            Image.asset(
+                              "src/img/planeIcon.png",
+                              fit: BoxFit.contain
+                            ),
+                            Text(
+                              flightsOptions.destination, 
+                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22)
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              shipDateFormated, 
+                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22)
+                            ),
+                            Text(
+                              "${flightsOptions.quantity} Tickets", 
+                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22)
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
           Positioned(
